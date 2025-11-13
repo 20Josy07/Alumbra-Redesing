1446712a40c54fe81c1381e0ae2f42d4d162e9bc
Josimar Acosta <josyacosta07@gmail.com>
2025-11-13 18:39:35 +0000

    Try fixing this error: `Runtime Error: Error: useEffect is not defined.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useUser } from "@/firebase";
 import { useRouter } from "next/navigation";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
