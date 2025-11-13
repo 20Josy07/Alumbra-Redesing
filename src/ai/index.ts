9d997673821b2e66658b0fa17089d5ca3eea9bf5
Josimar Acosta <josyacosta07@gmail.com>
2025-11-13 17:45:51 +0000

    me sigue saliendo el error de analisis y ya configure las vaariables de

---
+++ src/ai/index.ts
@@ -0,0 +1,5 @@
+// This file is used to register all the Genkit flows in a single place.
+// By importing this file in the layout, we ensure that the flows are
+// registered and available in the production environment.
+import './flows/analyze-text-input-for-abuse';
+import './flows/summarize-abuse-indicators';
